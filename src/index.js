54cf4f28d13ade9495a0ca5207a097286d301aed
Jeffrey <jefbfitzp33@outlook.com>
2023-11-05 02:25:11 -0800

    have DOM content loaded and fetch request.  Can't get fetch request to work though.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ document.addEventListener( 'DOMContentLoaded', ( ) => { } )
 
 console.log('JavaScript is working')
 console.log('testing git push')
+
+
+fetch("http//localhost:3000/movies")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(testing () {
+    
+  });
+
+  function testing(){
+    console.log("fetch works");
+
+  } 
